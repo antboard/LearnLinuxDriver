16e0379125cb039c9637d6d853bc52f3ec600080
icecut <icecut@qq.com>
2015-03-21 19:22:03 +0800

    添加输出语句
    比较高等级的输出.不用改系统输出等级

--- hello.c
+++ hello.c
@@ -4,6 +4,7 @@
 static int __init hello_init(void)
 {
 	/*驱动加载时会来此执行初始化代码*/
+	printk(KERN_ERR "icecut: hello_init\n");
 	return 0;
 }
 module_init(hello_init);
@@ -11,6 +12,7 @@ module_init(hello_init);
 static void __exit hello_exit(void)
 {
 	/*驱动卸载时会来此执行清理代码*/
+	printk(KERN_ERR "icecut: hello_exit\n");
 }
 module_exit(hello_exit);
 
